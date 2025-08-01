e28b12cabdddd0fd43517c9ad7a96f80d3d61be3
25LP3742 25LP3742 <Zoljargal.g247@gmail.com>
2025-08-01 12:36:54 +0800

    donation

--- src/controller/donation/get-donation-received.controller.ts
+++ src/controller/donation/get-donation-received.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
 
-export const getReceived = async ( req: Request, res: Response) => {
-    const { userId } = req.params;
-    const donation = await prisma.donation.findUnique({
-        where: { id: Number(userId) },
-    });
-    res.status(200).json({ donation });
-}
+export const getReceived = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  try {
+    const donations = await prisma.donation.findMany({
+    where: {
+      recipientId: Number(userId),
+    },
+  });
+  res.status(200).json({ donations });
+  } catch (error) {
+    res.status(500).json({error: "Хандивуудыг дуудахад алдаа гарлаа"})
+  }
+};
--- src/controller/donation/get-donation-total-earnings.controller.ts
+++ src/controller/donation/get-donation-total-earnings.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
 
-export const getTotalEarnings = async ( req: Request, res: Response) => {
-    const { userId } = req.params;
-    const donation = await prisma.donation.findUnique({
-        where: { id: Number(userId) },
+export const getTotalEarnings = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  try {
+    const donations = await prisma.donation.findMany({
+      where: { recipientId: Number(userId) },
     });
-    console.log("dd",donation);
-    
-    // const total = donation.reduce((sum, d) => sum + d.amount, 0);
-    res.status(200).json({  });
-}
+
+    const total = donations.reduce((sum, d) => sum + d.amount, 0);
+    res.status(200).json({ total });
+  } catch (error) {
+    res.status(500).json({error: "Нийт орлогыг тооцоход алдаа гарлаа"})
+  }
+};
