e3a6dbd9476fd7c73f003d4b28d6a2990acfb428
25LP9262 25LP9262 <25LP9262@pinecones-iMac.local>
2025-08-01 12:14:34 +0800

    bankcard fixed complete

--- src/controller/donation/create-donation.controller.ts
+++ src/controller/donation/create-donation.controller.ts
@@ -2,23 +2,28 @@ import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
 
 export const createDonation = async (req: Request, res: Response) => {
-    const {amount, specialMesssage, socialURLOrBuyMeACoffee, donorId, recipientId} = req.body;
-    try {
-        const donation = await prisma.donation.create({
-            data: {
-                amount,
-                specialMesssage,
-                socialURLOrBuyMeACoffee,
-                user:{connect:{
-                    id:Number(donorId)
-                }},
-                recipientId,
-            },
-        });
-        console.log("donation:", donation);
-        
-        res.status(200).json({ donation });
-    } catch (error) {
-        res.status(500).json({message: error})
-    }
-}
+  const {
+    amount,
+    specialMesssage,
+    socialURLOrBuyMeACoffee,
+    donorId,
+    recipientId,
+  } = req.body;
+  try {
+    // const donation = await prisma.donation.create({
+    //     data: {
+    //         amount,
+    //         specialMesssage,
+    //         socialURLOrBuyMeACoffee,
+    //         user:{connect:{
+    //             id:Number(donorId)
+    //         }},
+    //         recipientId,
+    //     },
+    // });
+    // console.log("donation:", donation);
+    // res.status(200).json({ donation });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
