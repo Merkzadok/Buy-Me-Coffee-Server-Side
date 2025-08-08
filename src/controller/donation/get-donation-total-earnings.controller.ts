eb1563520754224e94769b373b27e5dbd48482e6
25LP9262 25LP9262 <25LP9262@pinecones-iMac.local>
2025-08-08 10:16:14 +0800

    dateRange

--- src/controller/donation/get-donation-total-earnings.controller.ts
+++ src/controller/donation/get-donation-total-earnings.controller.ts
@@ -3,14 +3,23 @@ import { prisma } from "../../utils/prisma";
 
 export const getTotalEarnings = async (req: Request, res: Response) => {
   const { userId } = req.params;
+  const { from, to } = req.query;
   try {
+    const whereClause: any = { recipientId: Number(userId) };
+
+    if (from && to) {
+      whereClause.createdAt = {
+        gte: new Date(from as string),
+        lte: new Date(to as string),
+      };
+    }
     const donations = await prisma.donation.findMany({
-      where: { recipientId: Number(userId) },
+      where: whereClause,
     });
 
     const total = donations.reduce((sum, d) => sum + d.amount, 0);
     res.status(200).json({ total });
   } catch (error) {
-    res.status(500).json({error: "Нийт орлогыг тооцоход алдаа гарлаа"})
+    res.status(500).json({ error: "Нийт орлогыг тооцоход алдаа гарлаа" });
   }
 };
